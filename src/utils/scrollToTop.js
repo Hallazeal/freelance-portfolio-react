2425e06316893fbfcbf80427ec1a58a390f5add5
Nikita Danilov <danilov.ns@yandex.ru>
2024-08-21 14:33:04 +0300

    Initial commit

---
+++ src/utils/scrollToTop.js
@@ -0,0 +1,14 @@
+/* Возврат в начало при переключении страниц */
+
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
